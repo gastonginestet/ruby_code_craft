7bc74e041c833930f631b0e2b48cbb34f7da8a2f
Gaston Ginestet <gastonauginestet@gmail.com>
2024-04-20 00:38:09 -0300

    initialized gem creation

---
+++ lib/ruby_code_craft.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby_code_craft/version"
+
+module RubyCodeCraft
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ruby_code_craft_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyCodeCraft do
+  it "has a version number" do
+    expect(RubyCodeCraft::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
